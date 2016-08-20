a1e3ae050b2892cc24e9d1c85f0aca6bf1664abe
Pierre Oblin <pierre.oblin@orange.com>
2016-08-20 12:14:14 +0200

    add actuator configurable basic auth

---
+++ src/connectors/influxdb-nozzle/src/main/java/com/orange/oss/cloudfoundry/nozzle/config/WebSecurityConfig.java
@@ -0,0 +1,52 @@
+package com.orange.oss.cloudfoundry.nozzle.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+
+    @Value("${admin.user}")
+    private String adminUser;
+    @Value("${admin.password}")
+    private String adminPassword;
+	
+	
+
+    
+    
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser(adminUser).password(adminPassword).roles("ADMIN");
+    }
+
+
+
+    @Configuration
+    @Order(1)
+    public static class AdminSecurity extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .antMatcher("/admin/**")
+                    .authorizeRequests()
+                    .anyRequest()
+                    .hasRole("ADMIN")
+                    .and()
+                    .httpBasic()
+                    .and()
+                    .csrf().disable();
+        }
+    }
+    
+    
+    
+}
