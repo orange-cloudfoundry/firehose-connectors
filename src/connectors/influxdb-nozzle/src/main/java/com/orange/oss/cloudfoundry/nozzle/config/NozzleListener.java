cee7bbcf1f35551177e2646882174f7843a3e0c2
Pierre Oblin <pierre.oblin@orange.com>
2016-08-20 22:57:18 +0200

    add firehost credentials and snotel lib

---
+++ src/connectors/influxdb-nozzle/src/main/java/com/orange/oss/cloudfoundry/nozzle/config/NozzleListener.java
@@ -0,0 +1,39 @@
+package com.orange.oss.cloudfoundry.nozzle.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import cf.dropsonde.firehose.Firehose;
+import cf.dropsonde.firehose.FirehoseBuilder;
+
+@Configuration
+public class NozzleListener {
+
+	private static Logger logger = LoggerFactory.getLogger(NozzleListener.class.getName());
+
+	@Value("${firehose.url}")
+	String firehoseUrl;
+
+	@Value("${firehose.bearer}")
+	String firehoseBearer;
+
+	@Value("${firehose.skipTlsValidation}")
+	boolean firehoseSkipTlsValidation;
+
+	public NozzleListener() {
+		try (final Firehose firehose = FirehoseBuilder.create(this.firehoseUrl, this.firehoseBearer)
+				.skipTlsValidation(this.firehoseSkipTlsValidation).build()) {
+
+			logger.info("url:{}", this.firehoseUrl);
+			logger.info("bearer:" + this.firehoseBearer);
+			logger.info("skipTls:" + this.firehoseSkipTlsValidation);
+
+			firehose.open().toBlocking().forEach(envelope -> {
+				logger.info(envelope.toString());
+			});
+		}
+	}
+
+}
