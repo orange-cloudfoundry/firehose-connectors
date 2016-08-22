d247de84ecc8b8f4010fca65bcae5d07e60f3ced
Pierre Oblin <pierre.oblin@orange.com>
2016-08-22 11:49:20 +0200

    align firehose properties with snotel expected props cf.snotel.*

--- src/connectors/influxdb-nozzle/src/main/java/com/orange/oss/cloudfoundry/nozzle/InfluxdbNozzleApplication.java
+++ src/connectors/influxdb-nozzle/src/main/java/com/orange/oss/cloudfoundry/nozzle/InfluxdbNozzleApplication.java
@@ -3,7 +3,10 @@ package com.orange.oss.cloudfoundry.nozzle;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import cf.dropsonde.spring.boot.EnableFirehoseClient;
+
 @SpringBootApplication
+@EnableFirehoseClient
 public class InfluxdbNozzleApplication {
 
 	public static void main(String[] args) {
--- src/connectors/influxdb-nozzle/src/main/java/com/orange/oss/cloudfoundry/nozzle/config/NozzleListener.java
+++ src/connectors/influxdb-nozzle/src/main/java/com/orange/oss/cloudfoundry/nozzle/config/NozzleListener.java
@@ -2,6 +2,7 @@ package com.orange.oss.cloudfoundry.nozzle.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,27 +14,35 @@ public class NozzleListener {
 
 	private static Logger logger = LoggerFactory.getLogger(NozzleListener.class.getName());
 
-	@Value("${firehose.url}")
-	String firehoseUrl;
+	@Autowired
+	Firehose firehose;
 
-	@Value("${firehose.bearer}")
-	String firehoseBearer;
+	// public NozzleListener() {
+	// try (final Firehose firehose = FirehoseBuilder.create(this.firehoseUrl,
+	// this.firehoseBearer)
+	// .skipTlsValidation(this.firehoseSkipTlsValidation).build()) {
+	//
+	// logger.info("url:{}", this.firehoseUrl);
+	// logger.info("bearer:" + this.firehoseBearer);
+	// logger.info("skipTls:" + this.firehoseSkipTlsValidation);
+	//
+	// firehose.open().toBlocking().forEach(envelope -> {
+	// logger.info(envelope.toString());
+	// });
+	// }
+	// }
 
-	@Value("${firehose.skipTlsValidation}")
-	boolean firehoseSkipTlsValidation;
-
-	public NozzleListener() {
-		try (final Firehose firehose = FirehoseBuilder.create(this.firehoseUrl, this.firehoseBearer)
-				.skipTlsValidation(this.firehoseSkipTlsValidation).build()) {
-
-			logger.info("url:{}", this.firehoseUrl);
-			logger.info("bearer:" + this.firehoseBearer);
-			logger.info("skipTls:" + this.firehoseSkipTlsValidation);
-
-			firehose.open().toBlocking().forEach(envelope -> {
-				logger.info(envelope.toString());
-			});
-		}
+	public NozzleListener(){
 	}
-
+	
+	
+	public void startNozzle(){
+		logger.info("start nozzle");
+		firehose
+        .open()
+        .toBlocking()
+        .forEach(envelope -> {logger.info(envelope.toString());});
+	}
+	
+	
 }
